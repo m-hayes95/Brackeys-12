9efb3ae2b19b0f9cbc5bc169cb7e5305c5520803
MostAmbitiousCrow <sammywang54@gmail.com>
2024-09-11 14:18:51 +0100

    Animations Completed 90%

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -21,6 +23,7 @@ public class MainMenuManager : MonoBehaviour
     [Header("Pause Menu Info")]
     [SerializeField] GameObject pauseMenuObject;
     [SerializeField] bool canActivatePauseMenu;
+    [SerializeField] float timeScaleFadeTime = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -68,24 +71,62 @@ public class MainMenuManager : MonoBehaviour
         if (Input.GetButtonDown("Cancel") && canActivatePauseMenu)
         {
             pauseMenuObject.SetActive(true);
+            animator.SetBool("Is Main Menu", false);
+            animator.SetTrigger("Appear");
+            canActivatePauseMenu = false;
             Time.timeScale = 0;
         }
     }
 
     public void ResumeGame() // Pause Menu Resume Game
     {
-        pauseMenuObject.SetActive(false);
+        animator.SetTrigger("Disappear");
+        StartCoroutine(ResumeGameRoutine());
+    }
+
+    IEnumerator ResumeGameRoutine()
+    {
+        Time.timeScale = 0;
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.unscaledDeltaTime / timeScaleFadeTime;
+            Time.timeScale = t;
+            yield return null;
+        }
         Time.timeScale = 1;
+        canActivatePauseMenu = true;
+        pauseMenuObject.SetActive(false);
+        yield break;
     }
 
     public void EndGame() // Pause Menu End Game
     {
+        mainMenuActive = true;
+        canActivatePauseMenu = false;
         StartCoroutine(EndGameSequence());
     }
 
     IEnumerator EndGameSequence()
     {
         // Coroutine to stop whatever is going on (Placeholder atm, unsure if we even need this)
+        animator.SetTrigger("Disappear");
+        Debug.Log("Triggered Exit");
+
+        yield return new WaitForSecondsRealtime(2);
+        pauseMenuObject.SetActive(false);
+        mainMenuObject.SetActive(true);
+        mainMenuActive = true;
+        
+        Debug.Log("Triggered Menu");
+
+        // Reset Game Stats Here or trigger an event to do so!
+
+        animator.SetBool("Is Main Menu", true);
+        animator.SetTrigger("Appear");
+        Time.timeScale = 1;
+        Debug.Log("Triggered Menu");
+
         yield break;
     }
 }
