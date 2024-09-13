f7ff7a9756b3551c7584a7e7f17eb824c8c0d4c2
MostAmbitiousCrow <sammywang54@gmail.com>
2024-09-13 09:42:07 +0100

    Squashed commit of the following:
    commit dfbc0ce63428ce0f9dd6c7ea7babed94acfd2151
    Merge: 787d816 bfd94bf
    Author: MostAmbitiousCrow <sammywang54@gmail.com>
    Date:   Fri Sep 13 09:41:23 2024 +0100
    
        Merge branch 'Adding-sound-manager' into Temporary-Backup
    
    commit bfd94bfd4c407eea4f8736e73c222a2f6a7d829c
    Author: Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
    Date:   Thu Sep 12 00:16:22 2024 +0100
    
        Added Audio Manger to game with expandable functionality

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+        private Dictionary<AudioType, AudioSource[]> soundVariantsDictionary;
+        private Dictionary<AudioType, AudioSource> singleSoundDictionary;
+        [SerializeField] private AudioSource[] pigSoundsVar;
+        [SerializeField] private AudioSource[] mudSoundsVar;
+        [SerializeField] private AudioSource peacefulTrack;
+        [SerializeField] private AudioSource stormTrack;
+        [SerializeField] private AudioSource mudSound; // Used for testing single sound effect
+
+        #region Singleton Pattern
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+        #endregion
+
+        #region Dictionaries for Audio Types
+        private void Start()
+        {
+            PopulateSoundVariantsDictionaries();
+            PopulateSingleDictionaries();
+        }
+
+        private void PopulateSoundVariantsDictionaries()
+        {
+            soundVariantsDictionary = new Dictionary<AudioType, AudioSource[]>
+            {
+                { AudioType.PigSound_V, pigSoundsVar },
+                { AudioType.MudSound_V, mudSoundsVar }
+            };
+        }
+        private void PopulateSingleDictionaries()
+        {
+            singleSoundDictionary = new Dictionary<AudioType, AudioSource>
+            {
+                { AudioType.PeacefulTrack, peacefulTrack },
+                { AudioType.StormTrack, stormTrack }
+            };
+        }
+        #endregion
+
+        #region Control Sounds
+        public void PlaySound(AudioType audioType)
+        {
+            if (!soundVariantsDictionary.ContainsKey(audioType) || !singleSoundDictionary.ContainsKey(audioType))
+            { 
+                Debug.LogWarning($"Audio Type: {audioType} cannot be found. Check AudioType class for compatible names");
+                return;
+            }
+            // Play variant track of single track depending on audio type chosen
+            if (Enum.GetName(typeof(AudioType), audioType).EndsWith("V"))
+            {
+                AudioSource[] variants = soundVariantsDictionary[audioType];
+                RepeatSoundCheck(variants, variants.Length);
+            }
+            else if (!singleSoundDictionary[audioType].isPlaying)
+                singleSoundDictionary[audioType].Play();
+        }
+
+        public void StopSound(AudioType audioType)
+        {
+            if (!soundVariantsDictionary.ContainsKey(audioType) || !singleSoundDictionary.ContainsKey(audioType))
+            { 
+                Debug.LogWarning($"Audio Type: {audioType} cannot be found. Check AudioType class for compatible names");
+                return;
+            }
+            // Stop variant track of single track depending on audio type chosen
+            if (Enum.GetName(typeof(AudioType), audioType).EndsWith("V"))
+            {
+                AudioSource[] variants = soundVariantsDictionary[audioType];
+                foreach (AudioSource source in variants)
+                {
+                    source.Stop();
+                }
+            }
+            else if (singleSoundDictionary[audioType].isPlaying)
+                singleSoundDictionary[audioType].Stop();
+        }
+        #endregion
+        private int RepeatSoundCheck(AudioSource[] soundVariations, int range)
+        {
+            int index = Random.Range(0, range);
+            if (range == 1)
+                return index;
+            while (soundVariations[index].isPlaying)
+            {
+                index = Random.Range(0, range);
+            }
+            return index;
+        }
+        /*
+        public void PlayPigSounds()
+        {
+            int index = RepeatSoundCheck(pigSoundsVar, pigSoundsVar.Length);
+            pigSoundsVar[index].Play();
+        }
+
+        public void PlayMovingSound()
+        {
+            if (!mudSound.isPlaying)
+            {
+                mudSound.Play();
+            }
+        }
+
+        private void StopSound(AudioSource sound)
+        {
+            if (sound.isPlaying)
+                sound.Stop();
+        }
+        */
+        
+        
+    }
+}
+
---
+++ Assets/Scripts/Audio/AudioType.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Audio
+{
+    [Tooltip("Names ending in _V represent audio types with Variants")] public enum AudioType
+    {
+        PigSound_V,
+        MudSound_V,
+        PeacefulTrack,
+        StormTrack
+    }
+}
